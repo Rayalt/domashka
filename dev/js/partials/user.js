a14936afd39c2142c38e555c0369f00133d602d8
Alexander <moto.sasha@gmail.com>
2021-06-28 11:19:11 +0300

    ~ fixes

--- dev/js/partials/user.js
+++ dev/js/partials/user.js
@@ -9,9 +9,18 @@ window.onload = function() {
 			pageBody.classList.toggle('open-nav');
 		});
 	}
+
+	// scroll
+	document.querySelectorAll('a.main-nav__link[href^="#"]').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
 	
 	//inputmask
-
 	let inputList = document.querySelectorAll('[data-input-mask]');
 	for (let input of inputList) {
 		let im = new Inputmask("+7 (999) 999-99-99");
@@ -21,6 +30,5 @@ window.onload = function() {
 	//modal
 	const modals = new HystModal({
 		linkAttributeName: "data-hystmodal"
-		
 	});
-}
+};
